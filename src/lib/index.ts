26d4e5e588339215e6fcc04d6008ec90cb9c53f7
Martin Itterheim <itterheim.martin@gmail.com>
2025-05-09 16:14:10 +0200

    fix: module

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
 // Reexport your entry components here
+export { default as Card } from "./components/Card.svelte";
+export { default as Header } from "./components/Header.svelte";
+export { default as Icon } from "./components/Icon.svelte";
+export { default as IconButton } from "./components/IconButton.svelte";
+export { default as Layout } from "./components/Layout.svelte";
+export { default as Main } from "./components/Main.svelte";
+export { default as Nav } from "./components/Nav.svelte";
+export { default as NavLink } from "./components/NavLink.svelte";
+export { default as Wrapper } from "./components/Wrapper.svelte";
