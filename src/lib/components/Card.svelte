bff8be2c940a8051d2193fb42f14f8621ee3d16d
Martin Itterheim <itterheim.martin@gmail.com>
2025-05-09 14:47:36 +0200

    init

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    let {
+        title,
+        children,
+        actions
+    }: {
+        title?: string;
+        children: () => any;
+        actions?: () => any;
+    } = $props();
+</script>
+
+<div class="card">
+    {#if title}
+        <div class="header">
+            <div class="title">{title}</div>
+            <div class="actions">
+                {@render actions?.()}
+            </div>
+        </div>
+    {/if}
+    <div class="content">
+        {@render children()}
+    </div>
+</div>
+
+<style>
+    div.card {
+        border: 1px solid #0003;
+        border-radius: 10px;
+        overflow: hidden;
+        box-shadow: 1px 1px 3px 0px #0002;
+        background-color: var(--base);
+    }
+
+    div.header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 1px solid #0003;
+    }
+
+    div.title {
+        padding: 10px;
+        font-weight: 600;
+    }
+
+    div.actions {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        padding: 0 10px;
+    }
+
+    div.content {
+        padding: 10px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import Card from "$lib/components/Card.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import IconButton from "$lib/components/IconButton.svelte";
+    import Layout from "$lib/components/Layout.svelte";
+    import Main from "$lib/components/Main.svelte";
+    import Nav from "$lib/components/Nav.svelte";
+    import NavLink from "$lib/components/NavLink.svelte";
+    import Wrapper from "$lib/components/Wrapper.svelte";
+    import "$lib/styles.css";
+</script>
+
+<Layout>
+    <Nav>
+        {#snippet top()}
+            <NavLink href="/" icon="planet"></NavLink>
+        {/snippet}
+        {#snippet middle()}
+            <NavLink icon="stacked_email" label="Messages"></NavLink>
+            <NavLink icon="people" label="People" active={true}></NavLink>
+            <NavLink icon="book_2" label="Books"></NavLink>
+            <NavLink icon="directions_car" label="Car"></NavLink>
+        {/snippet}
+        {#snippet bottom()}
+            <NavLink href="#user" icon="person" label="user"></NavLink>
+        {/snippet}
+    </Nav>
+    <Main>
+        <Wrapper>
+            <Header title="Title">
+                <IconButton icon="add" action />
+                <IconButton icon="delete" danger />
+            </Header>
+            <Card title="Card">
+                {#snippet actions()}
+                    <IconButton icon="more_vert" small />
+                    <IconButton icon="add" action small />
+                    <IconButton icon="delete" danger small />
+                {/snippet}
+
+                And its content
+            </Card>
+        </Wrapper>
+    </Main>
+</Layout>
