3f4e256be8e13e4c09eb88f83158e4313f3359be
Martin Itterheim <itterheim.martin@gmail.com>
2025-05-09 15:17:25 +0200

    example

--- src/lib/stores/ui.ts
+++
@@ -1,7 +0,0 @@
-import { writable, type Writable } from "svelte/store";
-
-type Theme = "macchiato" | "latte"
-export const theme: Writable<Theme> = writable("latte");
-// theme.subscribe(value => localStorage.setItem("carlog.theme", value));
-
-export const showMenu = writable(false)
