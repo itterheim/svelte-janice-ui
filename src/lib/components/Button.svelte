b79f86190141b4d11e0e486a57fe24c4d5d26411
Martin Itterheim <itterheim.martin@gmail.com>
2025-05-10 10:42:44 +0200

    feat: button, input, row

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+    import Icon from "./Icon.svelte";
+
+    let {
+        submit,
+        label,
+        icon,
+        onclick,
+        action,
+        danger,
+        small
+    }: {
+        submit?: boolean;
+        label: string;
+        icon?: string;
+        onclick?: () => void;
+        action?: boolean;
+        danger?: boolean;
+        small?: boolean;
+    } = $props();
+</script>
+
+<button type={submit ? "submit" : "button"} class:action class:danger class:small {onclick}>
+    {#if icon}
+        <Icon {icon} />
+    {/if}
+    {label}
+</button>
+
+<style>
+    button {
+        background: none;
+        border: none;
+        padding: 0;
+        margin: 0;
+        cursor: pointer;
+
+        /* width: 40px; */
+        height: 40px;
+        border-radius: 5px;
+        padding: 0 14px;
+        font-size: var(--font-size);
+        font-family: "Open Sans", sans-serif;
+        font-weight: 600;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        color: inherit;
+
+        transition: ease-in-out 0.2s background-color;
+        border: 1px solid var(--border-color);
+    }
+
+    button.small {
+        /* width: 28px; */
+        height: 28px;
+        padding: 0 10px;
+    }
+
+    button:hover {
+        background-color: #0001;
+    }
+
+    button.action {
+        background-color: var(--primary);
+        color: var(--base);
+        transition: ease-in-out 0.2s filter;
+        box-shadow: 1px 1px 3px 0px #0002;
+        border: 1px solid transparent;
+    }
+
+    button.danger {
+        background-color: var(--red);
+        color: var(--base);
+        transition: ease-in-out 0.2s filter;
+        box-shadow: 1px 1px 3px 0px #0002;
+        font-weight: 700;
+        border: 1px solid transparent;
+    }
+
+    button.action:hover,
+    button.danger:hover {
+        filter: brightness(1.1);
+    }
+</style>
---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    let {
+        name,
+        label,
+        placeholder,
+        value = $bindable()
+    }: {
+        name: string;
+        value?: string;
+        label?: string;
+        placeholder?: string;
+    } = $props();
+</script>
+
+<div class="input">
+    <label for={name}>{label}</label>
+    <input type="text" id={name} {name} {placeholder} />
+</div>
+
+<style>
+    .input {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    label {
+        font-weight: 600;
+        /* padding-left: 10px; */
+    }
+
+    input {
+        font-size: var(--font-size);
+        padding: 10px;
+        border-radius: 5px;
+        border: 1px solid var(--border-color);
+        background-color: var(--base);
+        color: var(--text);
+    }
+</style>
