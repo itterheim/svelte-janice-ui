53e749032c515d6d6004eec813d155c0bee10f7d
Martin Itterheim <itterheim.martin@gmail.com>
2025-06-07 11:07:09 +0200

    feat: loader

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let { loading = false }: { loading?: boolean } = $props();
+</script>
+
+<div class="loader" class:hide={!loading}></div>
+
+<style>
+    div.loader {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 999999;
+
+        background-color: rgba(255,255,255,0.25);
+        backdrop-filter: blur(5px);
+    }
+
+    div.loader.hide {
+        display: none;
+    }
+</style>
