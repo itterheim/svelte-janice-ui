6f11d335501160f4d5ab8c9b0ead0ec68d808374
Martin Itterheim <itterheim.martin@gmail.com>
2025-07-19 19:28:03 +0200

    example

---
+++ _app/immutable/chunks/Bfa6RIcR.js
@@ -0,0 +1,1 @@
+import{l as o,d as a,b as t,m as c,g as u}from"./D_nMX8iu.js";function f(e){t===null&&o(),c&&t.l!==null?l(t).m.push(e):a(()=>{const n=u(e);if(typeof n=="function")return n})}function l(e){var n=e.l;return n.u??(n.u={a:[],b:[],m:[]})}export{f as o};
---
+++ _app/immutable/chunks/qw7hHicr.js
@@ -0,0 +1,1 @@
+import{o as a}from"./D_nMX8iu.js";a();
